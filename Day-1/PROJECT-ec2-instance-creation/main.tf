15a50dae57c023a70bf105e612cb0830aedc53ef
Balavardhan Sripuram <baalupec46@gmail.com>
2023-10-11 15:55:23 -0500

    Updated main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-053b0d53c279acc90"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    subnet_id = "subnet-00cbcc7e0178405d1"
+    key_name = "bsripuram"
 }
