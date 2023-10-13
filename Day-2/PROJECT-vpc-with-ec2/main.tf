14a176ee0e9f4ccfc6fa85c2e7568a451e222b07
Balavardhan Sripuram <baalupec46@gmail.com>
2023-10-12 23:56:03 -0500

    Update Day-2 project

--- Day-2/PROJECT-vpc-with-ec2/main.tf
+++ Day-2/PROJECT-vpc-with-ec2/main.tf
@@ -71,12 +71,12 @@ resource "aws_security_group" "webSg" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "abhisheksterraform2023project"
+  bucket = "balavardhansterraform2023project"
 }
 
 
 resource "aws_instance" "webserver1" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-053b0d53c279acc90"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
@@ -84,7 +84,7 @@ resource "aws_instance" "webserver1" {
 }
 
 resource "aws_instance" "webserver2" {
-  ami                    = "ami-0261755bbcb8c4a84"
+  ami                    = "ami-053b0d53c279acc90"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub2.id
